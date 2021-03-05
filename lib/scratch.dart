e0adedc22bcf2e02ab4f78540dc5882e8c1ad6ed
dungbk <conandung9999@gmail.com>
2021-03-05 11:49:50 +0700

    [add] completed demo, await

--- lib/scratch.dart
+++ lib/scratch.dart
@@ -2,23 +2,23 @@ void main() {
   performJob();
 }
 
-void performJob() {
+void performJob() async {
   task1();
-  task2();
-  task3();
+  String rs2 = await task2();
+  task3(rs2);
 }
 
 void task1() {
-  String result = 'task1 data';
+  // String result = 'task1 data';
   print('task1 completed!');
 }
 
-Future<String> task2() async {
+Future task2() async {
   String result;
   Duration threeSec = Duration(seconds: 3);
   // sleep(threeSec); // synchronous
 
-  Future.delayed(threeSec, () {
+  await Future.delayed(threeSec, () {
     result = 'task2 data';
     print('task2 completed!');
   });
@@ -26,7 +26,7 @@ Future<String> task2() async {
   return result;
 }
 
-void task3() {
-  String result = 'task3 data';
-  print('task3 completed!');
+void task3(String result2) {
+  // String result = 'task3 data';
+  print('task3 completed with $result2!');
 }
